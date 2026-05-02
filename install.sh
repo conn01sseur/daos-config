a4be4bf212b49e89793a40ac48a44d27b59e19cd
godsaveus <x1nder@proton.me>
2026-05-02 12:05:41 +0900

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+yay -S --needed \
+    hyprland hyprlock hyprpaper \
+    waybar rofi wofi kitty \
+    xdg-desktop-portal-hyprland xdg-utils \
+    qt5-wayland qt6-wayland \
+    pipewire pipewire-alsa pipewire-jack pipewire-pulse
+  wireplumber libpulse pavucontrol \
+    networkmanager network-manager-applet \
+    polkit-kde-agent \
+    grim slurp wl-clipboard \
+    jq imagemagick libnotify \
+    playerctl brightnessctl \
+    awww swww \
+    cava btop htop fastfetch \
+    zsh oh-my-zsh pokemon-colorscripts \
+    noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-
+  extra \
+    ttf-cascadia-code-nerd ttf-jetbrains-mono-nerd ttf-dejavu
+  ttf-nerd-fonts-symbols
